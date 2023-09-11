20885bc060bfbb10e59404bbf1b06b70dc2a827d
anerthy <112020687+anerthy@users.noreply.github.com>
2023-09-10 23:47:46 -0600

    build: se instala wouter para el enrutamiento de paginas

--- agro-market/src/App.jsx
+++ agro-market/src/App.jsx
@@ -1,77 +1,23 @@
-import React from "react";
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { Card } from "./components/Card";
-// import { ProductPage } from "./pages/ProductPage";
-import { PhotoList } from "./components/PhotoList";
-
-// import { Link, BrowserRouter, Route, Switch } from "react-router-dom";
-
-// const Actividades = () => <h1>Actividades</h1>;
+import { Route, Switch } from "wouter";
+import Home from "./components/Home";
+import Producers from "./components/Producers";
+import ProducerDetails from "./components/ProducerDetails";
+import Products from "./components/Products";
+// import ProductDetails from "./components/ProductDetails";
+import Activities from "./components/Activities";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const formatTitle = (title) => `@${title}`;
-  const card2 = {
-    fnt: formatTitle,
-    booleano: false,
-    title: "objeto",
-    body: "descripcion",
-    image: "https://placehold.co/100x100?text=objeto",
-  };
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      {/* <ProductsPage /> */}
-
-      <section className="card-section">
-        <Card
-          fnt={formatTitle}
-          booleano={false}
-          title="Hello world"
-          body="lorem ipsum"
-          image="https://placehold.co/100x100?text=Hello+World"
-        >
-          Hijo
-        </Card>
-
-        <Card {...card2}>Hijo</Card>
-
-        <Card
-          fnt={formatTitle}
-          booleano
-          body="lorem ipsum"
-          image="https://placehold.co/100x100?text=Hello+World"
-        >
-          Hijo
-        </Card>
-      </section>
-
-      <PhotoList />
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Switch>
+        <Route path="/" component={Home}></Route>
+        <Route path="/producers" component={Producers}></Route>
+        <Route path="/producer/:id" component={ProducerDetails}></Route>
+        <Route path="/products" component={Products}></Route>
+        {/* <Route path="/product/:id" component={ProductDetails}></Route> */}
+        <Route path="/activities" component={Activities}></Route>
+      </Switch>
+    </div>
   );
 }
 
