d43b2e02a4b6898836ab3b2dfc65fac7d5a99d5f
anerthy <112020687+anerthy@users.noreply.github.com>
2023-09-11 18:01:15 -0600

    se configura el confi

--- agromarket/Config/Config.php
+++ agromarket/Config/Config.php
@@ -1,30 +1,23 @@
 <?php
-const BASE_URL = "http://localhost/paraiso_azul";
+const BASE_URL = "http://localhost/agromarket";
 
 //Zona horaria
 date_default_timezone_set('America/Costa_Rica');
 
 //Datos de conexión a Base de Datos
 const DB_HOST = "localhost";
-const DB_NAME = "paraiso_azul";
+const DB_NAME = "agromarket";
 const DB_USER = "root";
 const DB_PASSWORD = "";
 const DB_CHARSET = "utf8";
 
-//Deliminadores decimal y millar Ej. 24,1989.00
-const SPD = ".";
-const SPM = ",";
-
-//Simbolo de moneda
-const SMONEY = "₡";
-
 //Datos envio de correo
-const NOMBRE_REMITENTE = "Paraíso Azul";
-const EMAIL_REMITENTE = "paraisoazulgolfonicoya@gmail.com";
-const NOMBRE_EMPESA = "Paraíso Azul";
-const WEB_EMPRESA = "paraisoazul.org";
+const NOMBRE_REMITENTE = "AgroMarket";
+const EMAIL_REMITENTE = "agromarket@mail.com";
+const NOMBRE_EMPESA = "AgroMarket";
+const WEB_EMPRESA = "agromarket.net";
 
 //Redes sociales
-const FACEBOOK = 'https://www.facebook.com/unacemede';
-const INSTAGRAM = 'https://www.instagram.com/unacemede/';
-const TWITTER = 'https://twitter.com/CemedeUna';
+const FACEBOOK = 'https://www.facebook.com/agromarket';
+const INSTAGRAM = 'https://www.instagram.com/agromarket/';
+const TWITTER = 'https://twitter.com/agromarket';
