54dbb2ce0a0da2934ee2a2924120c19effa60f9b
anerthy <112020687+anerthy@users.noreply.github.com>
2023-09-16 23:45:33 -0600

    se arregla el home

--- agromarket/Config/Config.php
+++ agromarket/Config/Config.php
@@ -1,5 +1,5 @@
 <?php
-const BASE_URL = "http://localhost/agromarket";
+const BASE_URL = "http://localhost/agromarket/agromarket";
 
 //Zona horaria
 date_default_timezone_set('America/Costa_Rica');
--- agromarket/Controllers/Home.php
+++ agromarket/Controllers/Home.php
@@ -1,9 +1,6 @@
 <?php
-require_once("Models/Traits/TContenidos.php");
-require_once("Models/Traits/TImagenes.php");
 class Home extends Controllers
 {
-	use TContenidos, TImagenes;
 	public function __construct()
 	{
 		parent::__construct();
@@ -15,8 +12,6 @@ class Home extends Controllers
 		$data['page_tag'] = "Home";
 		$data['page_title'] = "Página principal";
 		$data['page_name'] = "home";
-		$data['page_content'] = $this->getTextsInicio();
-		$data['page_media'] = $this->getImagesInicio();
 		$this->views->getView($this, "home", $data);
 	}
 }
