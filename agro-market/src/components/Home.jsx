82314276a37229cf16150f9ee9e1304f31d6333b
anerthy <112020687+anerthy@users.noreply.github.com>
2023-09-21 20:24:32 -0600

    se crea el navbar y footer

--- agro-market/src/components/Home.jsx
+++ agro-market/src/components/Home.jsx
@@ -1,16 +1,19 @@
-import Navbar from "./Navbar";
+import PageTemplate from "./PageTemplate";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <h1>AgroMarker</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt
-        cupiditate alias rem voluptas vero numquam pariatur architecto eum
-        deleniti minima reprehenderit, libero esse aperiam, sint repellendus,
-        necessitatibus laudantium quae earum.
-      </p>
+      <PageTemplate>
+        <div className="container text-center">
+          <h1>AgroMarker</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt
+            cupiditate alias rem voluptas vero numquam pariatur architecto eum
+            deleniti minima reprehenderit, libero esse aperiam, sint
+            repellendus, necessitatibus laudantium quae earum.
+          </p>
+        </div>
+      </PageTemplate>
     </>
   );
 }
--- agro-market/src/components/Navbar.jsx
+++ agro-market/src/components/Navbar.jsx
@@ -2,22 +2,36 @@ import { Link } from "wouter";
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link key={1} href={`/`}>
-        Inicio
-      </Link>
-      <br />
-      <Link key={2} href={`/products`}>
-        Productos
-      </Link>
-      <br />
-      <Link key={3} href={`/producers`}>
-        Productores
-      </Link>
-      <br />
-      <Link key={4} href={`/activities`}>
-        Actividades
-      </Link>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <Link to="/" className="navbar-brand">
+          AgroMarket
+        </Link>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/products" className="nav-link">
+                Productos
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/producers" className="nav-link">
+                Productores
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/activities" className="nav-link">
+                Actividades
+              </Link>
+            </li>
+            {/* <li className="nav-item">
+            <Link to="/donaciones" className="nav-link">
+              Donaciones
+            </Link>
+          </li> */}
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
