8330ad99ff70b345fd9d83a409faca6dd8672ccb
anerthy <112020687+anerthy@users.noreply.github.com>
2023-09-28 00:36:32 -0600

    vista base de afiliados

---
+++ agromarket/Views/Afiliado/afiliado.php
@@ -0,0 +1,21 @@
+<?php
+headerAdmin($data);
+?>
+<main class="app-content">
+    <div class="app-title">
+        <div>
+            <center>
+                <h1>Unirse al plan premium</h1>
+                <img src="https://placehold.co/300x200?text=Unete+a+nuestro+equipo" alt="imagen ilustrativa">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate dolorum temporibus iure officia quam necessitatibus cupiditate laborum animi explicabo voluptatem tempore eligendi, quae perspiciatis excepturi quo aliquam mollitia eveniet deleniti!</p>
+                <button>
+                    <a href="#">
+                        Volverme afiliado
+                    </a>
+                </button>
+            </center>
+        </div>
+    </div>
+</main>
+
+<?php footerAdmin($data); ?>
