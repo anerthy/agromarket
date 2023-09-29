4c2973a3166f9eab8cd694f5732e6f27ff4969f6
Sthone <melany.lawson.mendez@est.una.ac.cr>
2023-09-28 20:20:30 -0600

    vista de afiliado

--- agromarket/Views/Afiliado/afiliado.php
+++ agromarket/Views/Afiliado/afiliado.php
@@ -12,8 +12,8 @@ headerAdmin($data);
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         .custom-container {
-            height: 80vh; /* Establece la altura al 80% de la ventana */
-            width: 80vw; /* Establece el ancho al 80% de la ventana */
+            height: 100%; /* Establece la altura al 80% de la ventana */
+            width: 100%; /* Establece el ancho al 80% de la ventana */
             margin: auto; /* Centra el contenido vertical y horizontalmente */
             padding: 20px; /* Agrega un espacio alrededor del contenido */
             text-align: center; /* Centra el contenido horizontalmente */
@@ -22,18 +22,27 @@ headerAdmin($data);
 </head>
 <body>
     <div class="custom-container">
-        <h1>UNIRSE AL PLAN PREMIUM</h1>
+    
+        <h1> <strong> UNIRSE AL PLAN PREMIUM </strong></h1>
+        <img src="<?= media(); ?>/images/afiliado.jpg" alt="Afiliados" class="rounded" style="max-width: 40%; height: auto;">
         <br>
-        <img src="<?= media(); ?>/images/afiliados.jpg" alt="Afiliados" class="rounded" style="max-width: 40%; height: auto;">
-
-        <!-- <img src="<?= media(); ?>/images/afiliados.jpg" alt="Afiliados" class="img-fluid rounded"> -->
-        <p class="mt-3">En AgroMarket, valoramos la pasión por la agricultura y la dedicación a cultivar alimentos frescos y saludables. Si eres un productor de frutas y verduras comprometido con la calidad y la sostenibilidad, te invitamos a formar parte de nuestra comunidad.</p>
-        <a href="#" class="btn btn-primary mt-3">Solicitar Afiliación</a>
+        <br>
+        <h5>Beneficios del Plan Premium</h5>
+        <p class="mt-3">En AgroMarket hemos desarrollado un plan premium diseñado para potenciar al máximo tus ventajas en nuestra plataforma.
+             Te invitamos a aprovechar esta oportunidad y descubrir todas las ventajas que tenemos reservadas para ti por un costo mensual.
+        </p>
+        <!-- <ul class="list-inline text-center">
+        <li>Personalización del contenido avanzada</li>
+        <li>Funciones adicionales</li>
+        <li>Soporte al cliente mejorado</li>
+        </ul> -->
+        <a href="#" class="btn btn-success mt-3">Solicitar Afiliación</a>
     </div>
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    
 </body>
 </html> 
 
