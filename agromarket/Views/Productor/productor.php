793871b0b96fadfb0f3ad3fbfec67513fc32156f
FiorellaBonilla <fiorella.bonilla.gonzalez@est.una.ac.cr>
2023-09-28 19:48:14 -0600

    Cambiando aspecto del perfil

--- agromarket/Views/Productor/productor.php
+++ agromarket/Views/Productor/productor.php
@@ -1,92 +1,100 @@
 <?php
   headerAdmin($data);
-  getModal('modalProductor',$data);
- ?>
+  getModal('modalProductor', $data);
+?>
+
 <main class="app-content">
     <div class="row user">
         <div class="col-md-12">
             <div class="profile">
-                <div class="info"><img class="user-img" src="<?= media();?>/images/avatar.png">
-                
+                <div class="info">
+                    <img class="user-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSR6uEHi2L1HNWSAF99KaXalj_S1HfyvTjzKw&usqp=CAU" alt="Foto de perfil">
                 </div>
-                <div class="cover-image"></div>
+                <div class="cover-image" style="background-image: url('https://marketplace.canva.com/EAE8BWK0q3g/1/0/1600w/canva-fondo-de-pantalla-frutas-y-verduras-kawaii-naranja-Rtvd067L36k.jpg');">
+                </div>
             </div>
         </div>
         <div class="col-md-3">
             <div class="tile p-0">
                 <ul class="nav flex-column nav-tabs user-tabs">
-                    <li class="nav-item"><a class="nav-link active" href="#user-timeline" data-toggle="tab">Datos
+                    <li class="nav-item"><a class="nav-link active" href="#personal-info-tab" data-toggle="tab">Datos
                             personales</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#payment-methods-tab" data-toggle="tab">Métodos de
+                            pago</a></li>
                 </ul>
             </div>
         </div>
         <div class="col-md-9">
             <div class="tab-content">
-                <div class="tab-pane active" id="user-timeline">
-                    <div class="timeline-post">
-                        <div class="post-media">
-                            <div class="content">
-                                <h5>DATOS PERSONALES <button class="btn btn-sm btn-info" type="button"
-                                        onclick="openModalProductor();"><i class="fas fa-pencil-alt"
-                                            aria-hidden="true"></i></button></h5>
-                            </div>
-                        </div>
-
-                        <table class="table table-bordered">
-
-
+                <div class="tab-pane active" id="personal-info-tab">
+                    <!-- Contenido de la pestaña "Datos personales" -->
+                    <h5 class="mb-4">DATOS PERSONALES <button class="btn btn-sm btn-info" type="button" onclick="openModalProductor();"><i class="fas fa-pencil-alt" aria-hidden="true"></i></button></h5>
+                    <div class="table-responsive">
+                        <table id="personal-info-table" class="table table-striped table-bordered table-light table-sm">
+                            <!-- Agrega la clase table-sm para reducir el tamaño de la tabla -->
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>NOMBRE</th>
+                                    <th>CÉDULA</th>
+                                    <th>UBICACIÓN</th>
+                                    <th>ESTADO</th>
+                                </tr>
+                            </thead>
                             <tbody>
-                            
                                 <?php foreach ($data['arrData'] as $productor): ?>
-                                <tr>
-                                <td style="width:150px;">ID:</td>
-                                    <td><?php echo $productor['pdt_id']; ?></td>
-                                </tr>
-                                <tr>
-                                <td style="width:150px;">NOMBRE:</td>
-                                    <td><?php echo $productor['pdt_nombre']; ?></td>
-                                </tr>
-                                <tr>
-                                <td style="width:150px;">Cédula:</td>
-                                    <td><?php echo $productor['pdt_cedula']; ?></td>
-                                </tr>
-                                <tr>
-                                <td style="width:150px;">UBI:</td>
-                                    <td><?php echo $productor['pdt_ubicacion']; ?></td>
-                                </tr>
-
-                                <tr>
-                                <td style="width:150px;">EST:</td>
-                                    <td><?php echo $productor['pdt_estado']; ?></td>
-                                </tr>
-
-
+                                    <tr>
+                                        <td><?php echo $productor['pdt_id']; ?></td>
+                                        <td><?php echo $productor['pdt_nombre']; ?></td>
+                                        <td><?php echo $productor['pdt_cedula']; ?></td>
+                                        <td><?php echo $productor['pdt_ubicacion']; ?></td>
+                                        <td><?php echo $productor['pdt_estado']; ?></td>
+                                    </tr>
                                 <?php endforeach; ?>
-                            
-                                <tr>
-                                    <td colspan="5">No hay datos de productores disponibles.</td>
-                                </tr>
-                               
-
                             </tbody>
-
-
-
                         </table>
                     </div>
                 </div>
-
-                <div class="row mb-10">
-                    <div class="col-md-12">
-                        <button class="btn btn-primary" type="submit"><i class="fa fa-fw fa-lg fa-check-circle"></i>
-                            Guardar</button>
+                <div class="tab-pane" id="payment-methods-tab">
+                    <!-- Contenido de la pestaña "Métodos de pago" -->
+                    <h5 class="mb-4">MÉTODOS DE PAGO <button class="btn btn-sm btn-info" type="button" onclick="openModalProductor();"><i class="fas fa-pencil-alt" aria-hidden="true"></i></button></h5>
+                    <div class="table-responsive">
+                        <table id="payment-methods-table" class="table table-striped table-bordered table-light table-sm">
+                            <!-- Agrega la clase table-sm para reducir el tamaño de la tabla -->
+                            <thead>
+                                <tr>
+                                    <th>Sinpe Móvil</th>
+                                    <th>Transferencia bancaria</th>
+                                    <th>Paypal</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th>55555555</th>
+                                    <th>502568742024110</th>
+                                    <th>persona@hotmail.com</th>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
-                </form>
+            </div>
+            <div class="row mb-10">
+                <div class="col-md-12">
+                </div>
             </div>
         </div>
     </div>
-    </div>
-    </div>
 </main>
+
+
+
+<script>
+    // Inicializar DataTable en las tablas
+    $(document).ready(function() {
+        $('#personal-info-table').DataTable();
+        $('#payment-methods-table').DataTable();
+    });
+</script>
+
 <?php footerAdmin($data); ?>
