3c607fd6a2196f918aff67f808aa9bbd196aee44
anerthy <112020687+anerthy@users.noreply.github.com>
2023-09-29 21:31:57 -0600

    se le sa formato al codigo

--- agromarket/Controllers/Productor.php
+++ agromarket/Controllers/Productor.php
@@ -1,43 +1,39 @@
-<?php 
+<?php
 
-	class Productor extends Controllers{
-		public function __construct()
-		{
-			sessionStart();
-			parent::__construct();
-			$this->model = new ProductorModel();
-	
-		}	
+class Productor extends Controllers
+{
+	public function __construct()
+	{
+		sessionStart();
+		parent::__construct();
+		$this->model = new ProductorModel();
+	}
 
-		public function Productor(){
-    $data['page_tag'] = "Productor";
-    $data['page_title'] = "Productor";
-    $data['page_name'] = "productor";
-    $data['page_functions_js'] = "functions_productor.js";
-	$data['arrData'] = $this->model->selectUsuario(1);
-    $this->views->getView($this,"productor",$data); 
-}
+	public function Productor()
+	{
+		$data['page_tag'] = "Productor";
+		$data['page_title'] = "Productor";
+		$data['page_name'] = "productor";
+		$data['page_functions_js'] = "functions_productor.js";
+		$data['arrData'] = $this->model->selectUsuario(1);
+		$this->views->getView($this, "productor", $data);
+	}
 
 
-         
-		public function getProductor(int $pdt_id){
-                    $intIdProductor = intval(strClean($pdt_id));
-					if ($intIdProductor > 0) {
-						$arrData = $this->model->selectUsuario($intIdProductor);
-						if (empty($arrData)){
-							$arrResponse = array('status' => false, 'msg' => 'Datos no encontrados.');
-						} else {
-							$arrResponse = array('status' => true, 'data' => $arrData);
-						}
-						echo json_encode($arrData,JSON_UNESCAPED_UNICODE);
-					}
 
-			        die();
+	public function getProductor(int $pdt_id)
+	{
+		$intIdProductor = intval(strClean($pdt_id));
+		if ($intIdProductor > 0) {
+			$arrData = $this->model->selectUsuario($intIdProductor);
+			if (empty($arrData)) {
+				$arrResponse = array('status' => false, 'msg' => 'Datos no encontrados.');
+			} else {
+				$arrResponse = array('status' => true, 'data' => $arrData);
+			}
+			echo json_encode($arrData, JSON_UNESCAPED_UNICODE);
 		}
 
-		
-
-
-		
+		die();
 	}
- ?>
+}
