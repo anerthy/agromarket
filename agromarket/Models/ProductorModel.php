0a0349a2c0a53c8c7b008cba7a7f6687ee6c0427
anerthy <112020687+anerthy@users.noreply.github.com>
2023-10-02 22:44:07 -0600

    fix: se corrigen los metodos de insert y update
    la tabla de productores ya no tiene la columna id, se modifica para que coincida

--- agromarket/Models/ProductorModel.php
+++ agromarket/Models/ProductorModel.php
@@ -2,45 +2,73 @@
 
 class ProductorModel extends Mysql
 {
-	private $intIdProductor;
+	private $intIdUsuario;
 	private $strCedula;
 	private $strNombre;
 	private $strUbicacion;
 	private $strImage;
 	private $strEstado;
-	private $intIdUsuario;
 
 	public function __construct()
 	{
 		parent::__construct();
 	}
 
-
-	public function insertProductor(string $nombre, string $ubicacion, string $imagen)
+	public function getProductor(int $usuario, string $cedula)
 	{
+		$this->intIdUsuario = $usuario;
+		$this->strCedula 	= $cedula;
+		$sql = "SELECT
+					usr_id, 
+					per_cedula, 
+					pdt_nombre, 
+					pdt_ubicacion, 
+					pdt_imagen, 
+					pdt_estado
+				FROM productores
+				WHERE usr_id		= '{$this->intIdUsuario}'
+				  AND per_cedula	= '{$this->strCedula}'";
+		$request = $this->select_all($sql);
+		return $request;
+	}
 
+	public function insertProductor(
+		int $usuario,
+		string $cedula,
+		string $nombre,
+		string $ubicacion,
+		string $imagen
+	) {
 		$return = "";
-		$this->strNombre = $nombre;
+		$this->intIdUsuario = $usuario;
+		$this->strCedula 	= $cedula;
+		$this->strNombre 	= $nombre;
 		$this->strUbicacion = $ubicacion;
-		$this->strImage = $imagen;
+		$this->strImage 	= $imagen;
 
 
-		$sql = "SELECT pdt_nombre 
-					FROM productores 
-					WHERE act_nombre = '{$this->strNombre}' ";
+		$sql = "SELECT * 
+				FROM productores 
+				WHERE per_cedula = '{$this->strCedula}' ";
+
 		$request = $this->select_all($sql);
 
 		if (empty($request)) {
-			$query_insert  = "INSERT INTO productores
-								(pdt_nombre, 
-								pdt_ubicacion, 
-								pdt_imagen) 
-								VALUES(?,?,?)";
+			$query_insert  = 	"INSERT INTO productores(
+									usr_id,
+									per_cedula,
+									pdt_nombre,
+									pdt_ubicacion,
+									pdt_imagen,
+									pdt_estado
+								) VALUES('?','?','?','?','?','Activo')";
 
 			$arrData = array(
+				$this->intIdUsuario,
+				$this->strCedula,
 				$this->strNombre,
 				$this->strUbicacion,
-				$this->strImage,
+				$this->strImage
 			);
 			$request_insert = $this->insert($query_insert, $arrData);
 			$return = $request_insert;
@@ -50,82 +78,46 @@ class ProductorModel extends Mysql
 		return $return;
 	}
 
-	public function getProductor(int $usuario, string $cedula)
-	{
+	public function updateProductor(
+		int $usuario,
+		string $cedula,
+		string $nombre,
+		string $ubicacion,
+		string $imagen,
+		string $estado
+	) {
 		$this->intIdUsuario = $usuario;
 		$this->strCedula 	= $cedula;
-		$sql = "SELECT
-						per_cedula, 
-						pdt_nombre, 
-						pdt_ubicacion, 
-						pdt_imagen, 
-						usr_id, 
-						pdt_estado
-                    FROM productores
-					WHERE usr_id 		= $this->intIdUsuario
-					--   AND per_cedula 	= $this->strCedula  
-					";
-		$request = $this->select_all($sql);
-		return $request;
-	}
-
-	public function updateProductor(int $idProductor, string $cedula, string $nombre, string $ubicacion, string $imagen, string $estado)
-	{
-
-		$this->intIdProductor = $idProductor;
-		$this->strCedula = $cedula;
-		$this->strNombre = $nombre;
+		$this->strNombre 	= $nombre;
 		$this->strUbicacion = $ubicacion;
-		$this->strImage = $imagen;
-		$this->strEstado = $estado;
+		$this->strImage 	= $imagen;
+		$this->strEstado 	= $estado;
 
-		$sql = "SELECT * FROM productores WHERE (pdt_cedula = '{$this->strCedula}' ) ";
+		$sql = "SELECT * 
+				FROM productores 
+				WHERE pdt_cedula = '{$this->strCedula}'";
 		$request = $this->select_all($sql);
 
 		if (empty($request)) {
 
-			$sql = "UPDATE productores SET pdt_cedula=?, pdt_nombre=?, pdt_ubicacion=?, pdt_imagen=?, pdt_estado=? 
-							WHERE pdt_id = $this->intIdProductor ";
+			$sql = "UPDATE productores 
+					SET pdt_nombre		= ?, 
+						pdt_ubicacion 	= ?, 
+						pdt_imagen 		= ?, 
+						pdt_estado		= ? 
+					WHERE per_cedula 	= $this->strCedula 
+					  AND usr_id 		= $this->intIdUsuario";
 			$arrData = array(
-				$this->intIdProductor,
-				$this->strCedula,
 				$this->strNombre,
 				$this->strUbicacion,
 				$this->strImage,
 				$this->strEstado
 			);
 
-
 			$request = $this->update($sql, $arrData);
 		} else {
 			$request = "exist";
 		}
-		return $request;
-	}
-
-	public function updatePerfil(int $idProductor, string $cedula, string $nombre, string $ubicacion, string $imagen, string $estado)
-	{
-		$this->intIdProductor = $idProductor;
-		$this->strCedula = $cedula;
-		$this->strNombre = $nombre;
-		$this->strUbicacion = $ubicacion;
-		$this->strImage = $imagen;
-		$this->strEstado = $estado;
-
-
-
-		$sql = "UPDATE productores SET pdt_cedula=?, pdt_nombre=?, pdt_ubicacion=?, pdt_imagen=?, pdt_estado=? 
-				WHERE pdt_id = $this->intIdProductor ";
-		$arrData = array(
-			$this->intIdProductor,
-			$this->strCedula,
-			$this->strNombre,
-			$this->strUbicacion,
-			$this->strImage,
-			$this->strEstado
-		);
-
-		$request = $this->update($sql, $arrData);
 		return $request;
 	}
 }
