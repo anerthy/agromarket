de55924f155d8e14070f11cb53de7e1321fb4788
anerthy <112020687+anerthy@users.noreply.github.com>
2023-10-02 22:44:51 -0600

    se agrega el metodo volverseProductor para insert

--- agromarket/Controllers/Productor.php
+++ agromarket/Controllers/Productor.php
@@ -2,90 +2,116 @@
 
 class Productor extends Controllers
 {
-	public function __construct()
-	{
-		sessionStart();
-		parent::__construct();
+    public function __construct()
+    {
+        sessionStart();
+        parent::__construct();
 
-		if (empty($_SESSION['login'])) {
-			header('Location: ' . base_url() . '/login');
-		}
-	}
-
-	public function Productor()
-	{
-		$data['page_tag'] = "Productor";
-		$data['page_title'] = "Productor";
-		$data['page_name'] = "productor";
-		$data['page_functions_js'] = "functions_productor.js";
-		$data['arrData'] = $this->model->getProductor(
-			$_SESSION['userData']['usr_id'],
-			$_SESSION['userData']['per_cedula']
-		);
-		$this->views->getView($this, "productor", $data);
-	}
-
-
-	public function ProductorForm()
-{
-    // Inicializa las variables para que estén definidas
-    $nombre = "";
-    $ubicacion = "";
-    $imagen = "";
-
-    // Verifica si el formulario se ha enviado
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        // Obtén los valores de los campos del formulario
-        $nombre = strClean($_POST['txtNombre']);
-        $ubicacion = strClean($_POST['txtUbicacion']);
-        $imagen = $_FILES['foto']['name']; // Asegúrate de manejar correctamente la imagen
-
-        // Llama a la función insertProductor del modelo
-        $result = $this->model->insertProductor($nombre, $ubicacion, $imagen);
-
-        // Realiza cualquier otra lógica que necesites aquí, como manejar la respuesta de la inserción
-
-        // Redirige al usuario después de procesar el formulario
-        header('Location: productor.php');
-        exit;
+        if (empty($_SESSION['login'])) {
+            header('Location: ' . base_url() . '/login');
+        }
     }
 
-    // Resto del código para cargar la vista del formulario aquí
-    $data['page_tag'] = "Productor Form";
-    $data['page_title'] = "Productor Form";
-    $data['page_name'] = "productorform";
-    $data['page_functions_js'] = "functions_productor.js";
-    $this->views->getView($this, "productorform", $data);
-}
+    public function Productor()
+    {
+        $data['page_tag'] = "Productor";
+        $data['page_title'] = "Productor";
+        $data['page_name'] = "productor";
+        $data['page_functions_js'] = "functions_productor.js";
+        $data['arrData'] = $this->model->getProductor(
+            $_SESSION['userData']['usr_id'],
+            $_SESSION['userData']['per_cedula']
+        );
+        $this->views->getView($this, "productor", $data);
+    }
 
+    // sin params como se obtienen los datos de la SESSION
+    public function getProductor(/*int $usuario, string $cedula*/)
+    {
+        $arrData = $this->model->getProductor(
+            $_SESSION['userData']['usr_id'],
+            $_SESSION['userData']['per_cedula']
+        );
+        if (empty($arrData)) {
+            $arrResponse = array('status' => false, 'msg' => 'Datos no encontrados.');
+        } else {
+            $arrResponse = array('status' => true, 'data' => $arrData);
+        }
+        echo json_encode($arrData, JSON_UNESCAPED_UNICODE);
 
-	public function getProductor(int $pdt_id)
-	{
-		$intIdProductor = intval(strClean($pdt_id));
-		if ($intIdProductor > 0) {
-			$arrData = $this->model->selectUsuario($intIdProductor);
-			if (empty($arrData)) {
-				$arrResponse = array('status' => false, 'msg' => 'Datos no encontrados.');
-			} else {
-				$arrResponse = array('status' => true, 'data' => $arrData);
-			}
-			echo json_encode($arrData, JSON_UNESCAPED_UNICODE);
-		}
+        die();
+    }
 
-		die();
-	}
+    public function volverseProductor()
+    {
+        // la cedula y usuario se puede obtener del array SESSION
+        // $strUsuario     = strClean($_POST['txtUsuario']);
+        // $strCedula      = strClean($_POST['txtCedula']); 
+        $strNombre      = strClean($_POST['txtNombre']);
+        $strUbicacion   = strClean($_POST['txtUbicacion']);
+        $strImagen      = strClean($_POST['txtImagen']); // REVISAR OTROS CRUDS PARA VER EL MANEJO DE IMAGENES, YO NO SE JEJE, GL!
 
+        $request = $this->model->insertProductor(
+            $_SESSION['userData']['usr_id'],
+            $_SESSION['userData']['per_cedula'],
+            $strNombre,
+            $strUbicacion,
+            $strImagen
+        );
 
+        //! REVISAR LOS MENSAJES DE $ARRRESPONSE
+        if ($request > 0) {
+            $arrResponse = array('status' => true, 'msg' => 'Datos guardados correctamente.');
+        } else if ($request == 'exist') {
+            $arrResponse = array('status' => false, 'msg' => '¡Atención! Información ya existente en el sistema');
+        } else {
+            $arrResponse = array("status" => false, "msg" => 'No es posible almacenar los datos.');
+        }
+        echo json_encode($arrResponse, JSON_UNESCAPED_UNICODE);
+        die();
+    }
 
+    // ⚠ METODOS RESTANTES SIN REVISAR ⚠
+    public function ProductorForm()
+    {
+        // Inicializa las variables para que estén definidas
+        $nombre = "";
+        $ubicacion = "";
+        $imagen = "";
 
-	public function setProductor()
+        // Verifica si el formulario se ha enviado
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Obtén los valores de los campos del formulario
+            $nombre = strClean($_POST['txtNombre']);
+            $ubicacion = strClean($_POST['txtUbicacion']);
+            $imagen = $_FILES['foto']['name']; // Asegúrate de manejar correctamente la imagen
+
+            // Llama a la función insertProductor del modelo
+            $result = $this->model->insertProductor($nombre, $ubicacion, $imagen);
+
+            // Realiza cualquier otra lógica que necesites aquí, como manejar la respuesta de la inserción
+
+            // Redirige al usuario después de procesar el formulario
+            header('Location: productor.php');
+            exit;
+        }
+
+        // Resto del código para cargar la vista del formulario aquí
+        $data['page_tag'] = "Productor Form";
+        $data['page_title'] = "Productor Form";
+        $data['page_name'] = "productorform";
+        $data['page_functions_js'] = "functions_productor.js";
+        $this->views->getView($this, "productorform", $data);
+    }
+
+    public function setProductor()
     {
         $intIdProductor = intval($_POST['pdt_id']);
         $strNombre = strClean($_POST['txtNombre']);
         $strUbicacion = strClean($_POST['txtUbicacion']);
 
 
-    
+
         $foto       = $_FILES['foto'];
         $nombre_foto     = $foto['name'];
         $type              = $foto['type'];
@@ -98,14 +124,16 @@ class Productor extends Controllers
 
         if ($intIdActividad == 0) {
             // Crear
-            $request_Actividad = $this->model->insertActividad($strNombre,
-                                                              $strUbicacion,
-                                                              $imgImagen);
+            $request_Actividad = $this->model->insertActividad(
+                $strNombre,
+                $strUbicacion,
+                $imgImagen
+            );
             $option = 1;
         } else {
 
             // Actualizar
-             if ($nombre_foto == '') {
+            if ($nombre_foto == '') {
                 if ($_POST['foto_actual'] != 'imageUnavailable.png' && $_POST['foto_remove'] == 0) {
                     $imgImagen = $_POST['foto_actual'];
                 }
@@ -138,17 +166,4 @@ class Productor extends Controllers
         echo json_encode($arrResponse, JSON_UNESCAPED_UNICODE);
         die();
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
