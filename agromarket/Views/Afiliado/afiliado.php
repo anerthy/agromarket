1d1fbc1bebaf99c85b73f9413a766edccc7c4395
anerthy <112020687+anerthy@users.noreply.github.com>
2023-10-02 21:09:50 -0600

    seccion de ver-afiliacion

--- agromarket/Views/Afiliado/afiliado.php
+++ agromarket/Views/Afiliado/afiliado.php
@@ -1,35 +1,29 @@
 <?php
 headerAdmin($data);
 ?>
+
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .custom-container {
+            height: 100%;
+            /* Establece la altura al 80% de la ventana */
+            width: 100%;
+            /* Establece el ancho al 80% de la ventana */
+            margin: auto;
+            /* Centra el contenido vertical y horizontalmente */
+            padding: 20px;
+            /* Agrega un espacio alrededor del contenido */
+            text-align: center;
+            /* Centra el contenido horizontalmente */
+        }
+    </style>
+</head>
 <main class="app-content">
     <div class="app-title">
         <div>
-
-
-            <!DOCTYPE html>
-            <html>
-
-            <head>
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-                <style>
-                    .custom-container {
-                        height: 100%;
-                        /* Establece la altura al 80% de la ventana */
-                        width: 100%;
-                        /* Establece el ancho al 80% de la ventana */
-                        margin: auto;
-                        /* Centra el contenido vertical y horizontalmente */
-                        padding: 20px;
-                        /* Agrega un espacio alrededor del contenido */
-                        text-align: center;
-                        /* Centra el contenido horizontalmente */
-                    }
-                </style>
-            </head>
-
-            <body>
-                <div class="custom-container">
-
+            <div class="custom-container">
+                <section class="afiliarse">
                     <h1> <strong> UNIRSE AL PLAN PREMIUM </strong></h1>
                     <img src="<?= media(); ?>/images/afiliado.jpg" alt="Afiliados" class="rounded" style="max-width: 40%; height: auto;">
                     <br>
@@ -38,36 +32,21 @@ headerAdmin($data);
                     <p class="mt-3">En AgroMarket hemos desarrollado un plan premium diseñado para potenciar al máximo tus ventajas en nuestra plataforma.
                         Te invitamos a aprovechar esta oportunidad y descubrir todas las ventajas que tenemos reservadas para ti por un costo mensual.
                     </p>
-                    <!-- <ul class="list-inline text-center">
-        <li>Personalización del contenido avanzada</li>
-        <li>Funciones adicionales</li>
-        <li>Soporte al cliente mejorado</li>
-        </ul> -->
-
                     <button id="btn-afiliarse" class="btn btn-success mt-3" onClick="fntAfiliarse(1)">Solicitar Afiliación</button>
-
-                </div>
-
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-            </body>
-
-            </html>
-
-            <!-- <center>
-                <h1>Unirse al plan premium</h1>
-                <img src="https://placehold.co/300x200?text=Unete+a+nuestro+equipo" alt="imagen ilustrativa">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate dolorum temporibus iure officia quam necessitatibus cupiditate laborum animi explicabo voluptatem tempore eligendi, quae perspiciatis excepturi quo aliquam mollitia eveniet deleniti!</p>
-                <button>
-                    <a href="#">
-                        Volverme afiliado
-                    </a>
-                </button>
-            </center> -->
+                </section>
+                <hr>
+                <section class="ver-afiliacion">
+                    <h1>Andres Mejias</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla soluta eveniet veniam fuga esse quidem enim beatae, veritatis impedit temporibus ducimus vero praesentium eius iusto consequatur itaque ullam dolor dolorem.</p>
+                    <p>Afiliacion vigente hasta el <span>29/10/2023</span></p>
+                    <button class="btn btn-success mt-3">...</button>
+                </section>
+            </div>
         </div>
     </div>
 </main>
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <?php footerAdmin($data); ?>
