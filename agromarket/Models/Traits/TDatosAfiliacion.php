7cfb7e8d36ca68d6dece077b0ee4224927f8fdec
anerthy <112020687+anerthy@users.noreply.github.com>
2023-10-04 18:54:58 -0600

    se crea el trait

---
+++ agromarket/Models/Traits/TDatosAfiliacion.php
@@ -0,0 +1,17 @@
+<?php
+require_once("Libraries/Core/Mysql.php");
+trait TDatosAfiliacion
+{
+    public $con;
+
+    public function existeAfiliado()
+    {
+        $this->con = new Mysql();
+        $sql = "SELECT COUNT(*) AS EXISTE
+                FROM afiliados
+                WHERE usr_id = '{$_SESSION['userData']['usr_id']}' 
+                  AND per_cedula = '{$_SESSION['userData']['per_cedula']}';";
+        $request = $this->con->select_all($sql);
+        return $request;
+    }
+}
