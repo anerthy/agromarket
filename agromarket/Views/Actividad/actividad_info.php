62ad3c88bdbad2ac6e38b9cc8e8e3694897d7900
Sthone <melany.lawson.mendez@est.una.ac.cr>
2023-10-11 21:46:50 -0600

    la vista de actividades para aprobación

--- agromarket/Views/Actividad/actividad_info.php
+++ agromarket/Views/Actividad/actividad_info.php
@@ -119,92 +119,78 @@
     <div class="container-xxl py-5">
         <div class="container">
             <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-                <p class="fs-5 fw-bold text-primary">Our Projects</p>
-                <h1 class="display-5 mb-5">Some Of Our Wonderful Projects</h1>
+                <p class="fs-5 fw-bold text-primary">Nuestras Actividades</p>
+                <h1 class="display-5 mb-5">Actividades a Realizar</h1>
             </div>
             <div class="row wow fadeInUp" data-wow-delay="0.3s">
                 <div class="col-12 text-center">
                     <ul class="list-inline rounded mb-5" id="portfolio-flters">
-                        <li class="mx-2 active" data-filter="*">All</li>
-                        <li class="mx-2" data-filter=".first">Complete Projects</li>
-                        <li class="mx-2" data-filter=".second">Ongoing Projects</li>
+                        <li class="mx-2 active" data-filter="*">Todas</li>
+                        <li class="mx-2" data-filter=".first">Actividades Completas</li>
+                        <li class="mx-2" data-filter=".second">Actividades en Curso</li>
                     </ul>
                 </div>
             </div>
+
             <div class="row g-4 portfolio-container">
-                <div class="col-lg-4 col-md-6 portfolio-item first wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="portfolio-inner rounded">
-                        <img class="img-fluid" src="<?= media(); ?>/images/img/plantilla/img/service-1.jpg" alt="">
-                        <div class="portfolio-text">
-                            <h4 class="text-white mb-4">Landscaping</h4>
-                            <div class="d-flex">
-                                <a class="btn btn-lg-square rounded-circle mx-2" href="<?= media(); ?>/images/img/plantilla/img/service-1.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-                                <a class="btn btn-lg-square rounded-circle mx-2" href=""><i class="fa fa-link"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 portfolio-item second wow fadeInUp" data-wow-delay="0.3s">
-                    <div class="portfolio-inner rounded">
-                        <img class="img-fluid" src="<?= media(); ?>/images/img/plantilla/img/service-2.jpg" alt="">
-                        <div class="portfolio-text">
-                            <h4 class="text-white mb-4">Pruning plants</h4>
-                            <div class="d-flex">
-                                <a class="btn btn-lg-square rounded-circle mx-2" href="<?= media(); ?>/images/img/plantilla/img/service-2.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-                                <a class="btn btn-lg-square rounded-circle mx-2" href=""><i class="fa fa-link"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 portfolio-item first wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="portfolio-inner rounded">
-                        <img class="img-fluid" src="<?= media(); ?>/images/img/plantilla/img/service-3.jpg" alt="">
-                        <div class="portfolio-text">
-                            <h4 class="text-white mb-4">Irrigation & Drainage</h4>
-                            <div class="d-flex">
-                                <a class="btn btn-lg-square rounded-circle mx-2" href="<?= media(); ?>/images/img/plantilla/img/service-3.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-                                <a class="btn btn-lg-square rounded-circle mx-2" href=""><i class="fa fa-link"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 portfolio-item second wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="portfolio-inner rounded">
-                        <img class="img-fluid" src="<?= media(); ?>/images/img/plantilla/img/service-4.jpg" alt="">
-                        <div class="portfolio-text">
-                            <h4 class="text-white mb-4">Garden Maintenance</h4>
-                            <div class="d-flex">
-                                <a class="btn btn-lg-square rounded-circle mx-2" href="<?= media(); ?>/images/img/plantilla/img/service-4.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-                                <a class="btn btn-lg-square rounded-circle mx-2" href=""><i class="fa fa-link"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 portfolio-item first wow fadeInUp" data-wow-delay="0.3s">
-                    <div class="portfolio-inner rounded">
-                        <img class="img-fluid" src="<?= media(); ?>/images/img/plantilla/img/service-5.jpg" alt="">
-                        <div class="portfolio-text">
-                            <h4 class="text-white mb-4">Green Technology</h4>
-                            <div class="d-flex">
-                                <a class="btn btn-lg-square rounded-circle mx-2" href="<?= media(); ?>/images/img/plantilla/img/service-5.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-                                <a class="btn btn-lg-square rounded-circle mx-2" href=""><i class="fa fa-link"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 portfolio-item second wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="portfolio-inner rounded">
-                        <img class="img-fluid" src="<?= media(); ?>/images/img/plantilla/img/service-6.jpg" alt="">
-                        <div class="portfolio-text">
-                            <h4 class="text-white mb-4">Urban Gardening</h4>
-                            <div class="d-flex">
-                                <a class="btn btn-lg-square rounded-circle mx-2" href="<?= media(); ?>/images/img/plantilla/img/service-6.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-                                <a class="btn btn-lg-square rounded-circle mx-2" href=""><i class="fa fa-link"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    <div class="col-lg-4 col-md-6 portfolio-item first wow fadeInUp" data-wow-delay="0.1s">
+        <div class="card">
+            <i class="fa fa-leaf fa-5x d-block mx-auto mt-3 text-success"></i>
+            <div class="card-body">
+                <h5 class="card-title">Evento 1</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in volutpat nunc. Vivamus quis dui at risus fermentum auctor.</p>
+                <p><i class="fa fa-map-marker"></i> Lugar: Ciudad Evento 1</p>
+                <p><i class="fa fa-calendar"></i> Fecha: 01/01/2023</p>
+                <p><i class="fa fa-clock"></i> Hora: 10:00 AM</p>
             </div>
+        </div>
+    </div>
+   <div class="col-lg-4 col-md-6 portfolio-item first wow fadeInUp" data-wow-delay="0.1s">
+        <div class="card">
+            <i class="fa fa-leaf fa-5x d-block mx-auto mt-3 text-success"></i>
+            <div class="card-body">
+                <h5 class="card-title">Evento 1</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in volutpat nunc. Vivamus quis dui at risus fermentum auctor.</p>
+                <p><i class="fa fa-map-marker"></i> Lugar: Ciudad Evento 1</p>
+                <p><i class="fa fa-calendar"></i> Fecha: 01/01/2023</p>
+                <p><i class="fa fa-clock"></i> Hora: 10:00 AM</p>
+            </div>
+        </div>
+    </div>
+    <!-- Repite el mismo patrón para las demás tarjetas -->
+    <div class="col-lg-4 col-md-6 portfolio-item first wow fadeInUp" data-wow-delay="0.1s">
+        <div class="card">
+            <i class="fa fa-leaf fa-5x d-block mx-auto mt-3 text-success"></i>
+            <div class="card-body">
+                <h5 class="card-title">Evento 1</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in volutpat nunc. Vivamus quis dui at risus fermentum auctor.</p>
+                <p><i class="fa fa-map-marker"></i> Lugar: Ciudad Evento 1</p>
+                <p><i class="fa fa-calendar"></i> Fecha: 01/01/2023</p>
+                <p><i class="fa fa-clock"></i> Hora: 10:00 AM</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-md-6 portfolio-item first wow fadeInUp" data-wow-delay="0.1s">
+        <div class="card">
+            <i class="fa fa-leaf fa-5x d-block mx-auto mt-3 text-success"></i>
+            <div class="card-body">
+                <h5 class="card-title">Evento 1</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in volutpat nunc. Vivamus quis dui at risus fermentum auctor.</p>
+                <p><i class="fa fa-map-marker"></i> Lugar: Ciudad Evento 1</p>
+                <p><i class="fa fa-calendar"></i> Fecha: 01/01/2023</p>
+                <p><i class="fa fa-clock"></i> Hora: 10:00 AM</p>
+            </div>
+        </div>
+    </div>
+
+
+    </div>
+
+
+
+
+
         </div>
     </div>
     <!-- Projects End -->
