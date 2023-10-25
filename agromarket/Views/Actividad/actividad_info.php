8025acc0693ae516b3344a28ea04b8cb3643f7a1
Sthone <melany.lawson.mendez@est.una.ac.cr>
2023-10-25 12:07:15 -0600

    se ajusta la imagen y se hace responsive

--- agromarket/Views/Actividad/actividad_info.php
+++ agromarket/Views/Actividad/actividad_info.php
@@ -94,20 +94,20 @@
 
             <div class="row g-4 portfolio-container">
 
-                <?php foreach ($data['listado_actividades'] as $actividad) : ?>
-                    <div class="col-lg-4 col-md-6 portfolio-item <?php echo $actividad['act_lugar']; ?> wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="card">
-                            <i class="fa fa-leaf fa-5x d-block mx-auto mt-3 text-success"></i>
-                            <div class="card-body">
-                                <img src="<?php echo media() . '/images/uploads/actividades/' . $actividad['act_imagen']; ?>" alt="<?php echo $actividad['act_nombre']; ?>" width="100px">
-                                <h5 class="card-title"><?php echo $actividad['act_nombre']; ?></h5>
-                                <p class="card-text"><?php echo $actividad['act_descripcion']; ?></p>
-                                <p><i class="fa fa-map-marker"></i> Lugar: <?php echo $actividad['act_lugar']; ?></p>
-                                <p><i class="fa fa-calendar"></i> Fecha: <?php echo $actividad['act_fecha']; ?></p>
-                            </div>
-                        </div>
-                    </div>
-                <?php endforeach; ?>
+            <?php foreach ($data['listado_actividades'] as $actividad) : ?>
+    <div class="col-lg-4 col-md-6 portfolio-item <?php echo $actividad['act_lugar']; ?> wow fadeInUp" data-wow-delay="0.1s">
+        <div class="card">
+            <div class="card-body">
+                <img class="card-img img-fluid" src="<?php echo media() . '/images/uploads/actividades/' . $actividad['act_imagen']; ?>" alt="<?php echo $actividad['act_nombre']; ?>">
+                <h5 class="card-title"><?php echo $actividad['act_nombre']; ?></h5>
+                <p class="card-text"><?php echo $actividad['act_descripcion']; ?></p>
+                <p><i class="fa fa-map-marker"></i> Lugar: <?php echo $actividad['act_lugar']; ?></p>
+                <p><i class="fa fa-calendar"></i> Fecha: <?php echo $actividad['act_fecha']; ?></p>
+            </div>
+        </div>
+    </div>
+<?php endforeach; ?>
+
 
             </div>
         </div>
